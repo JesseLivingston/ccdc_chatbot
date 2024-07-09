09037f2c30b19dc80c66608012a6f30feddf201b
Chen Fan <jesse.livermore@me.com>
2024-07-09 19:35:32 +0800

    fuck

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -5,6 +5,12 @@ import ChatBotDiscussionComposer from './ChatBotDiscussionComposer';
 
 app.initializers.add('ccdc/chatbot', () => {
   console.log("[ccdc/chatbot] Hello, forum!!");
+  if (!app.composer) {
+      app.composer = {};
+  }
+  if (!app.composer.components) {
+      app.composer.components = {};
+  }
   app.composer.components.discussion = ChatBotDiscussionComposer;
   console.log('set app.composer.components.discussion to ChatBotDiscussionComposer');
   extend(PostUser.prototype, 'view', function (view) {
